1b538e9b947f34b2da1b8617001b81fba4817a2c
GiacomoArena <giacomoarena28@gmail.com>
2023-10-05 16:10:41 +0200

    add style

--- php/changePsw.php
+++ php/changePsw.php
@@ -16,11 +16,24 @@ require_once(__DIR__.'/config.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Aggiungi il resto delle tue intestazioni HTML -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- font family -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,600;0,700;0,800;1,400&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,600;0,9..40,700;1,9..40,400&family=Fredoka:wght@300;400;500;600;700&family=Montserrat:wght@400;500;600&family=Orbitron:wght@400;600;800&family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,400;1,600&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../assets/styles/style.css">
+        <title>Edusogno</title>
+    
 </head>
 <body>
     <header>
-        <!-- Aggiungi il tuo codice HTML per l'intestazione -->
+        <div class="container">
+            <a href="../index.php">
+                <img src="../assets/img/logo.png" alt="">
+            </a>
+        </div>
     </header>
     <main>
         <section class='form-section'>
@@ -59,10 +72,16 @@ require_once(__DIR__.'/config.php');
         </section>
     
         <span class="img-container">
-            <!-- Aggiungi il tuo codice HTML per le immagini -->
+          <img class="cerchio" src="../assets/img/cerchio.png" alt="">
+          <img class="prima" src="../assets/img/1.png" alt="">
+          <img class="seconda" src="../assets/img/2.png" alt="">
+          <img class="terza" src="../assets/img/3.png" alt="">
+          <img class="mezzaluna" src="../assets/img/mezzaluna.png" alt="">
+          <img class="razzo" src="../assets/img/razzo.png" alt="">
         </span>
     </main>
     
     <script src="../assets/js/script.js"></script>
 </body>
 </html>
+
--- php/recoveryPassword.php
+++ php/recoveryPassword.php
@@ -62,17 +62,17 @@ if ($stmt->execute()) {
               $mail->Body = 'Clicca sul link per cambiare la tua password. <a href="http://localhost/esercizio-edusogno/edusogno-esercizio/php/changePsw.php?email=' . $user['email'] . '&token=' . $token . '">Procedi con il cambio password</a>';
 
               $mail->send();
-              echo 'Procedi con il cambio password cliccando al link inviato tramite mail</a>';
+              echo '<span class="message">Procedi con il cambio password cliccando al link inviato tramite mail &nbsp; <a href="../index.php">torna alla Home</a></span>';
               exit();
           } catch (Exception $e) {
               echo 'Errore nell\'invio dell\'email: ' . $mail->ErrorInfo;
           }
       } else {
-          echo 'Errore durante il salvataggio del token nel database.';
+          echo '<span class="message">Errore durante il salvataggio del token nel database.</span>';
           exit();
       }
   } else {
-      echo '<span>non è presente nessuna mail nel database &nbsp;</span><a href="../index.php">torna indietro</a>';
+      echo '<span class="message">non è presente nessuna mail nel database &nbsp; <a href="../index.php">torna indietro</a></span>';
   }
 } else {
   echo "Errore durante l'esecuzione della query.";
@@ -80,3 +80,46 @@ if ($stmt->execute()) {
 // ...
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- font family -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,400;0,600;0,700;0,800;1,400&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,600;0,9..40,700;1,9..40,400&family=Fredoka:wght@300;400;500;600;700&family=Montserrat:wght@400;500;600&family=Orbitron:wght@400;600;800&family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,400;1,600&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../assets/styles/style.css">
+        <title>Edusogno</title>
+    </head>
+
+<body>
+    
+    <header>
+        <div class="container">
+            <a href="../index.php">
+                <img src="../assets/img/logo.png" alt="">
+            </a>
+            
+        </div>
+    </header>
+
+  <main>
+    
+      
+      <img class="cerchio" src="../assets/img/cerchio.png" alt="">
+      <img class="prima" src="../assets/img/1.png" alt="">
+      <img class="seconda" src="../assets/img/2.png" alt="">
+      <img class="terza" src="../assets/img/3.png" alt="">
+      <img class="mezzaluna" src="../assets/img/mezzaluna.png" alt="">
+      <img class="razzo" src="../assets/img/razzo.png" alt="">
+    
+  </main>
+
+<script src="assets/js/script.js"></script>
+</body>
+
+</html>
+
